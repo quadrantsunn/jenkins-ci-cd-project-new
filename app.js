368765833db01b7f2d3aa1d55778d7256ada5eda
quadrantsunn <revurilaxman7@gmail.com>
2026-09-10 17:43:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get("/api/contacts", (req, res) => {
     res.json(readContacts());
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Application running on port ${PORT}`);
